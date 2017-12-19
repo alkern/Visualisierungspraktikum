21aa2f560177e972b0f177d8b680d3a42e32c29c
Visprak30 <visprak30@cantor.informatik.uni-leipzig.de>
2017-12-19 16:20:28 +0100

    Have to redo triangulate. Voronoi intersections may work.

--- Voronoi.cpp
+++ Voronoi.cpp
@@ -21,7 +21,7 @@ namespace
             {
                  add<TensorFieldContinuous<2, Vector2>>("Field", "Feld mit Input" );
                  add< DefaultValueArray<std::pair<Point2, CriticalPointType>>>( "Critical Points", "List of critical points");
-
+                 add< Color >("Color", "Farbe VoronoLinien", Color(0.7, 0.9, 0.9));
             }
         };
 
@@ -55,19 +55,72 @@ namespace
             mGlyphs->add(Primitive::POINTS).setColor(Color(0.0,1.0,0.0)).setPointSize(3).setVertices(criticalPoints3D);
 
             // figuring out 3 closest points to each other
-            std::vector<std::vector<Point3>> closestPairList;
-            closestPairList = threeNeighbourPointsList(criticalPoints3D);
+            std::vector<std::vector<Point3>> closestTripletList;
+            closestTripletList = threeNeighbourPointsList(criticalPoints3D);
 
             //find the cutting points between triangles of closest pairs
-            std::vector<Point3> middlePoints;
-            for(size_t i = 0; i < closestPairList.size(); i++){
-                Point3 p1 = closestPairList[i][0];
-                Point3 p2 = closestPairList[i][1];
-                Point3 p3 = closestPairList[i][2];
-                middlePoints.push_back(getMiddle(p1,p2));
-                middlePoints.push_back(getMiddle(p1,p3));
-                middlePoints.push_back(getMiddle(p2,p3));
+            std::vector<std::vector<Point3>> middlePoints;
+            for(size_t i = 0; i < closestTripletList.size(); i++){
+                Point3 p1 = closestTripletList[i][0];
+                Point3 p2 = closestTripletList[i][1];
+                Point3 p3 = closestTripletList[i][2];
+                std::vector<Point3> thisMiddlePoints;
+                thisMiddlePoints.push_back(getMiddle(p1,p2));
+                thisMiddlePoints.push_back(getMiddle(p1,p3));
+                thisMiddlePoints.push_back(getMiddle(p2,p3));
+                middlePoints.push_back(thisMiddlePoints);
+
+                //uncomment to check middle Points
+                debugLog()<< "m1" << getMiddle(p1,p2) <<std::endl;
+                debugLog()<< "m2" << getMiddle(p1,p3) << std::endl;
+                debugLog()<< "m3" << getMiddle(p2,p3) << std::endl;
             }
+
+            // draw the Voronoi lines
+            std::vector<Point3> circleMiddlePoints;
+            for(size_t i = 0; i < closestTripletList.size(); i++){
+                Point3 p1 = closestTripletList[i][0];
+                Point3 p2 = closestTripletList[i][1];
+                Point3 p3 = closestTripletList[i][2];
+                Point3 m1 = middlePoints[i][0]; //p1,p2
+                Point3 m2 = middlePoints[i][1]; //p1,p3
+
+                Vector3 criticalPointToMiddlePointM1 = m1-p1;
+                Vector3 criticalPointToMiddlePointM2 = m2-p1;
+                Vector3 orthogonalVectorM1(-criticalPointToMiddlePointM1[1], criticalPointToMiddlePointM1[0], 0);
+                Vector3 orthogonalVectorM2(-criticalPointToMiddlePointM2[1], criticalPointToMiddlePointM2[0], 0);
+
+                //calculating intersect of m1 + a*Ortho1 = m2 + b*Ortho2
+                double a1 = m1[0];
+                double b1 = m2[0];
+                double a2 = m1[1];
+                double b2 = m2[1];
+                double r1 = orthogonalVectorM1[0];
+                double r2 = orthogonalVectorM1[1];
+                double s1 = orthogonalVectorM2[0];
+                double s2 = orthogonalVectorM2[1];
+
+                //to fix
+//                double b = (r2*(x1-x2)-r1*(y1-y2))/(r2*s1-r1*s2);
+//                double a = (y2+ b +s2 -y1)/r2;
+//                Point3 circleMiddle(x1+a*r1, y1+a*r2, 0);
+
+                double v = (r2*(a1-b1) - r1*(a2-b2)) / (r2*s1 - r1*s2);
+                Point3 circleMiddle(b1 + v * s1, b2 + v * s2, 0);
+
+//                circleMiddlePoints.push_back(circleMiddle);
+//                circleMiddlePoints.push_back(m1);
+//                circleMiddlePoints.push_back(m2);
+                circleMiddlePoints.push_back(p1);
+                circleMiddlePoints.push_back(p2);
+                circleMiddlePoints.push_back(p3);
+           }
+
+           mGlyphs->add(Primitive::TRIANGLES).setColor(options.get< Color >("Color")).setVertices(circleMiddlePoints);
+
+
+
+
         }
 
 
@@ -82,10 +135,9 @@ namespace
                 //firstPoint
                 Point3 currentPointToFindNeighbour = pList[i];
 
-                //works now
                 std::vector<std::tuple<Point3,double>> pointDistanceTupleList;
                 for(size_t j = 0; j < pList.size(); j++){
-                    std::tuple<Point3,double> pointDistanceTuple(pList[j], getDistance(currentPointToFindNeighbour, pList[j]));
+                    std::tuple<Point3,double> pointDistanceTuple(pList[j], norm(currentPointToFindNeighbour - pList[j]));
                     pointDistanceTupleList.push_back(pointDistanceTuple);
                 }
 
@@ -99,6 +151,7 @@ namespace
                         closestPoint = std::get<0>(pointDistanceTupleList[j]);
                     }
                 }
+
                 // find second closest one ; refactor later
                 double secondShortestDistance = std::numeric_limits<double>::max();
                 Point3 secondClosestPoint = Point3(0,0,0);
@@ -119,23 +172,18 @@ namespace
                 threeNeighbourPoints.push_back(currentPointToFindNeighbour);
                 threeNeighbourPoints.push_back(closestPoint);
                 threeNeighbourPoints.push_back(secondClosestPoint);
+
                 result.push_back(threeNeighbourPoints);
             }
             return result;
         }
 
-
-        //CONTINUE HERE
         //calculates the middle between 2 points
         Point3 getMiddle(Point3 p1, Point3 p2){
-            return Point3(0,0,0);
-        }
-
-        //takes 2 Point3s and returns the distance as double
-        double getDistance(Point3 p1, Point3 p2){
-            double result = sqrt(pow((p2[0]-p1[0]), 2.0)+pow((p2[1]-p1[1]), 2.0));
+            Point3 result = p1 + ((p2-p1)/2) ;
             return result;
         }
+
 
         //take 2D Point and returns it in 3D
         Point3 to3D(Point2 p) {
